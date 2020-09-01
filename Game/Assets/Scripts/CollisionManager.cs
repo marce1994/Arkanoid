3d58341d09f235f7f2247a67aaafb0e6b50bb636
pablo <358126+marce1994@users.noreply.github.com>
2020-09-01 11:06:40 -0300

    Implementando colisiones

---
+++ Game/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    private static CollisionManager collisionManager;
+    private List<CustomCollider2D> customCollider2Ds;
+
+    public bool collideSameLayer;
+
+    public CollisionManager GetInstance() {
+        if (collisionManager == null)
+            collisionManager = this;
+        else
+            Destroy(this.gameObject);
+        return collisionManager;
+    }
+
+    public void RegisterCollider(CustomCollider2D customCollider2D) {
+        if (customCollider2Ds == null)
+            customCollider2Ds = new List<CustomCollider2D>();
+        
+        if (customCollider2Ds.Contains(customCollider2D))
+            return;
+
+        customCollider2Ds.Add(customCollider2D);
+    }
+
+    public void UnregisterCollider(CustomCollider2D customCollider2D) {
+        if (customCollider2Ds == null)
+            return;
+
+        customCollider2Ds.Remove(customCollider2D);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
--- Game/Assets/Scripts/CustomCollider2D.cs
+++ Game/Assets/Scripts/CustomCollider2D.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-
+using UnityEditor;
 public enum ColliderType
 {
     Sphere,
@@ -15,8 +15,31 @@ public class CustomCollider2D : MonoBehaviour
     public ColliderType ColliderType;
     public event Action onCollisionEnter;
 
-    private void FixedUpdate()
+    public float width = 0.0f;
+    public float heigth = 0.0f;
+
+    public float radius = 0.0f;
+
+    public Vector3 centerOffset;
+
+    private void OnDrawGizmos()
     {
-        // Physics
+        Gizmos.color = Color.green;
+
+        switch (ColliderType)
+        {
+            case ColliderType.Sphere:
+                {
+                    Gizmos.DrawWireSphere(transform.position + centerOffset, radius);
+                }
+                break;
+            case ColliderType.AABB:
+                {
+                    Gizmos.DrawWireCube(transform.position + centerOffset, new Vector3(width, heigth, 0));
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
